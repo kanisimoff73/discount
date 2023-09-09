125851645d455a7ae20825fdcefa0240efb8bd87
kirill <kanisimoff73@gmail.com>
2023-09-09 15:07:13 +0700

    Добавил файл main_app url.py для взятия из него адресов URL. Добавил панель debug toolbar.

--- website/website/urls.py
+++ website/website/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from website import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main_app.urls'))
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [path('__debug__/', include(debug_toolbar.urls)),] + urlpatterns
