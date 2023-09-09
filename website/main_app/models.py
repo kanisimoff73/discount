b3ba12e0486c33677598ea15186bfef5a45887ec
kirill <kanisimoff73@gmail.com>
2023-09-09 15:12:26 +0700

    Добавлены модели базы данных и класс 'Meta' для отображения названий в админке.

--- website/main_app/models.py
+++ website/main_app/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Shops(models.Model):
+    objects = None
+    name = models.CharField(max_length=20, db_index=True, verbose_name="Магазины")
+    slug = models.SlugField(max_length=200, unique=True, db_index=True, verbose_name="URL")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("shops", kwargs={"shop_slug": self.slug})
+
+    class Meta:
+        verbose_name_plural = "Магазины"
+
+
+class Categories(models.Model):
+    objects = None
+    name = models.CharField(max_length=20, db_index=True, verbose_name="Категория")
+    slug = models.SlugField(max_length=200, unique=True, db_index=True, verbose_name="URL")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Категории"
+
+
+class Products(models.Model):
+    objects = None
+    name = models.TextField(blank=True, db_index=True, verbose_name="Название")
+    photo = models.TextField(verbose_name="Фото")
+    price = models.FloatField(verbose_name="Цена")
+    link = models.TextField(verbose_name='Ссылка', null=True)
+    cat = models.ForeignKey("Categories", on_delete=models.CASCADE, verbose_name="Категории")
+    shop = models.ForeignKey("Shops", on_delete=models.CASCADE, verbose_name="Магазины")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Продукты"
