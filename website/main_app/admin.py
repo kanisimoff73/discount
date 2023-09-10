bd5394874415b8382eb5d80f66da6882c55ae581
kirill <kanisimoff73@gmail.com>
2023-09-10 13:21:23 +0700

    Создал модели отображения в админке.

--- website/main_app/admin.py
+++ website/main_app/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class ShopsAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug")
+    list_display_links = ("id", "name", "slug")
+    search_fields = ("name", "slug")
+
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "slug")
+    list_display_links = ("id", "name", "slug")
+    search_fields = ("name", "slug")
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "photo", "price", "link", "cat", "shop")
+    list_display_links = ("id", "name", "photo", "price", "link", "cat", "shop")
+
+
+admin.site.register(Shops, ShopsAdmin)
+admin.site.register(Categories, CategoriesAdmin)
+admin.site.register(Products, ProductsAdmin)
